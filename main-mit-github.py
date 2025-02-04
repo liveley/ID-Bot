c5bf341f27a473e07ddc51c42d0338fd79f5a965
Lilly Schutz <l.schutz@hm.edu>
2025-02-04 18:23:16 +0100

    test with github

---
+++ main-mit-github.py
@@ -0,0 +1,92 @@
+import os
+import time
+import gradio as gr
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage, PromptTemplate
+from llama_index.llms.openai import OpenAI
+from llama_index.core import Settings
+
+from theme import CustomTheme
+
+path_modulhandbuch = "./modulhandbuch"
+path_persist = os.path.join(path_modulhandbuch, "persist")
+
+client = OpenAI(
+    base_url="https://models.inference.ai.azure.com",
+    api_key=os.environ["GITHUB_TOKEN"],
+)
+
+#Settings.llm = OpenAI(temperature=0.1, model="gpt-4o-mini")
+
+if not os.path.exists(path_persist):
+    documents = SimpleDirectoryReader("./modulhandbuch/").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist(persist_dir=path_persist)
+else:
+    storage_context = StorageContext.from_defaults(persist_dir=path_persist)
+    index = load_index_from_storage(storage_context)
+
+template = (
+    "We have provided context information below. \n"
+    "---------------------\n"
+    "{context_str}"
+    "\n---------------------\n"
+    "Given only this information and without using ur general knowledge, please answer the question in the style of Shakespeare and always answer in german: {query_str}\n"
+)
+qa_template = PromptTemplate(template)
+query_engine = index.as_query_engine(streaming=True, text_qa_template=qa_template)
+
+
+def response(message, history):
+    streaming_response = query_engine.query(message)
+
+    #answer = ""
+    #for text in streaming_response.response_gen:
+    #    time.sleep(0.05)
+    #    answer += text
+    #    yield answer
+
+    response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "",
+        },
+        {
+            "role": "user",
+            "content": {message},
+        }
+    ],
+    model="gpt-4o",
+    temperature=1,
+    max_tokens=4096,
+    top_p=1
+    )
+    yield response.choices[0].message.content
+
+    
+
+
+theme = CustomTheme()
+
+def main():
+    chatbot = gr.Chatbot(
+        value=[{"role": "assistant", "content": "How can I help you today?"}],
+        type="messages",
+        show_label=False,
+        avatar_images=("./avatar_images/human.png", "./avatar_images/robot.png"),
+        elem_id="CHATBOT"
+    )
+
+    chatinterface = gr.ChatInterface(
+        fn=response,
+        chatbot=chatbot,
+        type="messages",
+        theme=theme,
+        css_paths="./style.css"
+    )
+
+    chatinterface.launch(inbrowser=True)
+
+
+if __name__ == "__main__":
+    main()
